75760e1a6a35c675f536e569407ca2083bd287f0
Rana Junaid <rjunaid5@gmail.com>
2017-02-28 16:09:52 +0500

    Changes from remote

--- custom/modules/Cases/logic_hooks.php
+++ custom/modules/Cases/logic_hooks.php
@@ -6,7 +6,7 @@ $hook_array = Array();
 // position, file, function 
 $hook_array['before_save'] = Array(); 
 $hook_array['before_save'][] = Array(1, 'Cases push feed', 'modules/Cases/SugarFeeds/CaseFeed.php','CaseFeed', 'pushFeed'); 
-
+//Changed
 
 
 ?>
